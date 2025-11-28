64e815725ad7fb2ec346959f59c43eb385d3a237
monholm <73996878+monholm@users.noreply.github.com>
2025-11-28 10:30:17 +0100

    fix: disable git-checks when using pnpm (#19)

--- packages/npm-registry/src/index.ts
+++ packages/npm-registry/src/index.ts
@@ -42,6 +42,16 @@ export default async function publish(publishDetails: PublishDetail[]) {
           access,
           '--tag',
           tag,
+          /**
+           * Pnpm checks whether we are on the `publish branch` (defaults to `master` or `main`)
+           * and whether the local branch is clean and up-to-date with the remote.
+           * Since we are doing releases from other branches and in most cases have uncommitted changes
+           * (the version bumps in package.json), we need to disable these checks.
+           * Future improvement could be to set the `publish branch` to the current branch,
+           * and check whether there are other changes than the ones we are about to publish.
+           * See: https://pnpm.io/cli/publish
+           */
+          '--no-git-checks',
         ];
 
         await spawn('pnpm', args, undefined);
