e3c034bd128a764f2090202ffad231e557f74b5f
monholm <73996878+monholm@users.noreply.github.com>
2025-11-28 08:57:07 +0100

    feat: add support for pnpm

--- packages/core/src/utils/workspaces.ts
+++ packages/core/src/utils/workspaces.ts
@@ -1,23 +1,57 @@
 import {glob} from 'tinyglobby';
 import {loadPkgJson} from './pkgJson.js';
 import {join} from 'node:path';
+import {parse} from 'yaml';
 import type {addBumpLevels} from './conventionalCommit.js';
+import type {PackageManager} from '../types.js';
+import {readFile} from 'node:fs/promises';
+import {isObject, isStringArray} from '@monolease/util';
 
 export async function matchWorkspacesDirs(globs: string[], rootDir: string) {
   return glob(globs, {cwd: rootDir, absolute: true, onlyDirectories: true});
 }
 
-export async function listWorkspaces(rootDir: string) {
-  const rootPkgJson = await loadPkgJson(join(rootDir, 'package.json'));
+interface ListWorkspacesOptions {
+  rootDir: string;
+  packageManager: PackageManager;
+}
 
-  if (!rootPkgJson.workspaces) {
-    throw new Error('No workspaces found in root package.json');
-  }
+export async function listWorkspaces({
+  rootDir,
+  packageManager,
+}: ListWorkspacesOptions) {
+  const workspacesGlobs = await (async () => {
+    switch (packageManager) {
+      case 'npm':
+      case 'yarn': {
+        const rootPkgJson = await loadPkgJson(join(rootDir, 'package.json'));
 
-  const workspacesDirs = await matchWorkspacesDirs(
-    rootPkgJson.workspaces,
-    rootDir,
-  );
+        if (!rootPkgJson.workspaces) {
+          throw new Error('No workspaces found in root package.json');
+        }
+        return rootPkgJson.workspaces;
+      }
+
+      case 'pnpm': {
+        const pnpmWorkspaceYamlPath = join(rootDir, 'pnpm-workspace.yaml');
+        const file = await readFile(pnpmWorkspaceYamlPath, 'utf8');
+        const pnpmWorkspace = parse(file) as unknown;
+        if (
+          !isObject(pnpmWorkspace) ||
+          !isStringArray(pnpmWorkspace.packages)
+        ) {
+          throw new Error('No packages found in pnpm-workspace.yaml');
+        }
+        return pnpmWorkspace.packages;
+      }
+      default:
+        packageManager satisfies never;
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        throw new Error(`Unsupported package manager: ${packageManager}`);
+    }
+  })();
+
+  const workspacesDirs = await matchWorkspacesDirs(workspacesGlobs, rootDir);
 
   return Promise.all(
     workspacesDirs.map(async dir => {
